57a670c03980ebb549d5b73abcaf8e5ca8ed3432
Deepanshu Demla <ddemla@techracers.io>
2019-12-19 14:10:05 +0530

    Added build controls layout

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,7 +3,10 @@ import './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(ig => [...Array(props.ingredients[ig])].map((_, i) => <BurgerIngredient key={ig + i} type={ig} />))
+    let transformedIngredients = Object.keys(props.ingredients).map(ig => [...Array(props.ingredients[ig])].map((_, i) => <BurgerIngredient key={ig + i} type={ig} />)).reduce((arr, el) => arr.concat(el), []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredient</p>
+    }
     console.log(transformedIngredients)
     return (
         <div className="Burger">
